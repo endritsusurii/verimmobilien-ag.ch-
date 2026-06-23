4bf3fb2149f4116a13dc7ce3566bc4b2ea194b4c
Endrit Susuri <endritsusuri008@gmail.com>
2026-06-23 21:03:38 +0200

    link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'VER Immobilien AG')</title>
 
-    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-    <script src="{{ asset('build/assets/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
+    <script src="{{ asset('resources/js/app.js') }}" defer></script>
 
     <style>
         :root {
